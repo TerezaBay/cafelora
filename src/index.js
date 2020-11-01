27aa2447af2e39d71a3a1efac18177b385e01666
Tereza Bayerová <49003875+TerezaBay@users.noreply.github.com>
2020-11-01 16:21:52 +0100

    Smazání nepotřebného pole, vše funkční

--- src/index.js
+++ src/index.js
@@ -20,44 +20,6 @@ navLinks.forEach((navLink) => {
   });
 });
 
-// pole s údaji pro jednotlivé nápoje pro komponenty Layer, LayerList, Drink a DrinkList
-const drinkMeta = [
-  {
-    id: 'cappuccino',
-    name: 'Cappuccino',
-    ordered: false,
-    layers: [
-      {
-        color: '#feeeca',
-        label: 'mléčná pěna',
-      },
-      {
-        color: '#fed7b0',
-        label: 'teplé mléko',
-      },
-      {
-        color: '#613916',
-        label: 'espresso',
-      },
-    ],
-  },
-  {
-    id: 'romano',
-    name: 'Romano',
-    ordered: false,
-    layers: [
-      {
-        color: '#fbdf5b',
-        label: 'citrón',
-      },
-      {
-        color: '#613916',
-        label: 'espresso',
-      },
-    ],
-  },
-];
-
 // načtení API se seznamem nápojů a jejich předání komponentě DrinkList
 const fetchDrinks =() => {
   fetch("http://cafelora.kodim.cz/api/drinks").then((response) => response.json()).then((data) => {
