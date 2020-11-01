3324f1a283c43762c960983b916cb540d5108d31
Tereza Bayerová <49003875+TerezaBay@users.noreply.github.com>
2020-11-01 11:56:20 +0100

    Funkčnost tlačítka objednávky se zručením

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './style.css';
 
 console.log('funguju!');
 
+// rozbalování navigace pro mobil
 const nav = document.querySelector('nav');
 const navLinks = document.querySelectorAll('nav a');
 
@@ -15,3 +16,20 @@ navLinks.forEach((navLink) => {
     nav.classList.toggle('nav-closed');
   });
 });
+
+// objednávání nápojů
+const orderBtn = document.querySelector(".order-btn")
+const drinkCupElm = document.querySelector(".drink__cup")
+let ordered = false
+
+orderBtn.addEventListener("click", () => {
+if (ordered === false) {
+    drinkCupElm.classList.add(".drink__cup--selected")
+    orderBtn.textContent = "Zrušit"
+    ordered = true
+  } else {
+    drinkCupElm.classList.remove(".drink__cup--selected")
+    orderBtn.textContent = "Objednat"
+    ordered = false
+  }
+})
